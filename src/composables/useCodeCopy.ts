16ccd1d521e735ef5fb19f061b22aff5210a7360
Aimony <aimony@qq.com>
2025-12-21 13:19:11 +0800

    feat: 优化代码框折叠动效

--- src/composables/useCodeCopy.ts
+++ src/composables/useCodeCopy.ts
@@ -19,7 +19,7 @@ export function useCodeCopy() {
       // Provide feedback
       const originalHtml = target.innerHTML
       target.innerHTML =
-        '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-green-500"><polyline points="20 6 9 17 4 12"></polyline></svg>'
+        '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-green-500"><polyline points="20 6 9 17 4 12"></polyline></svg>'
 
       setTimeout(() => {
         target.innerHTML = originalHtml
--- src/plugins/markdown-it-code-enhancements.ts
+++ src/plugins/markdown-it-code-enhancements.ts
@@ -15,16 +15,22 @@ export const codeEnhancementsPlugin = (md: MarkdownIt) => {
 
     return `
 <div class="code-block-wrapper">
-  ${lang ? `<span class="code-lang">${lang}</span>` : ''}
-  <div class="mac-controls">
-    <div class="mac-icon mac-icon-red"></div>
-    <div class="mac-icon mac-icon-yellow"></div>
-    <div class="mac-icon mac-icon-green"></div>
+  <div class="code-block-header">
+    <div class="mac-controls">
+      <div class="mac-icon mac-icon-red"></div>
+      <div class="mac-icon mac-icon-yellow"></div>
+      <div class="mac-icon mac-icon-green"></div>
+    </div>
+    <div class="code-header-right">
+      ${lang ? `<span class="code-lang">${lang}</span>` : ''}
+      <button class="code-copy-btn" title="Copy Code">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
+      </button>
+    </div>
   </div>
-  <button class="code-copy-btn" title="Copy Code">
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
-  </button>
-  ${rawCode}
+  <div class="code-block-content">
+    ${rawCode}
+  </div>
 </div>
 `
   }
